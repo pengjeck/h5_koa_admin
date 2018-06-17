6709658c46c9a6a2bcdb33b9ee205ca884c24ac7
pjzero <1823403153@qq.com>
2018-06-17 20:20:01 +0800

    [test pass] most interface

--- router/index.js
+++ router/index.js
@@ -5,7 +5,7 @@ const shopctrl = require('../controllers/shops');
 router
     .get('/api/1.0/shop/findAll', shopctrl.findAll)  // shop
     .post('/api/1.0/shop/add', shopctrl.add)
-    .post('/api/1.0/shop/add', shopctrl.modify)
+    .post('/api/1.0/shop/modify', shopctrl.modify)
     .post('/api/1.0/shop/delete', shopctrl.delete)
     .post('/api/1.0/user/register', userctrl.register)   // user
     .post('/api/1.0/user/login', userctrl.login)
